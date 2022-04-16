d77e4ec53943dcbbcb7ec0f2320b0e8df1554b3c
muhammad <timurkaevmuhammad4@gmail.com>
2022-04-16 15:12:01 +0300

    get audio files 2

--- App.jsx
+++ App.jsx
@@ -3,13 +3,20 @@ import { Provider } from 'react-redux';
 import { store } from './redux/index'
 import AppRoutes from './AppRoutes';
 import { NavigationContainer } from '@react-navigation/native';
+import AudioListItem from './components/SendMaterial/AudioListScreen/components/AudioListItem';
+import { View } from 'react-native';
 
 export default function App() {
+  // return (
+  //   <Provider store={store}>
+  //     <NavigationContainer>
+  //      <AppRoutes />
+  //     </NavigationContainer>
+  //   </Provider>
+  // );
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-       <AppRoutes />
-      </NavigationContainer>
-    </Provider>
+    <View style={{marginTop: 50}}>
+      <AudioListItem />
+    </View>
   );
 }
